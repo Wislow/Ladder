8d4c73ca47011d2ac4aaff924ce27c5610dd49a7
Jiyoon <juoon.hwang@yahoo.com>
2014-10-06 09:21:26 +0900

    Row Class edited

--- Ladder/src/ladder/Ladder.java
+++ Ladder/src/ladder/Ladder.java
@@ -1,33 +1,27 @@
 package ladder;
 
 public class Ladder {
-	int[][] matrix;
+	Row[] tempRows;
 	
 	Ladder(int rows, int entry){
-		matrix = new int[rows][entry];
-	}
-	
-	public static void main(String[] args) {
-
+		tempRows = new Row[rows];
+		for (int i=0 ; i<rows ; i++) {
+			tempRows[i] = new Row(entry);
+		}
 	}
 
 	public int run(int startPoint) {
-		for (int[] row : matrix) {
-			if (row[startPoint] == 0) {
-				continue;
-			}
-			if (startPoint < (row.length-1) && row[startPoint + 1] == 1) {
-				startPoint += 1;
-				continue;
-			}
-			startPoint -= 1;
-			continue;
+		for (Row row : tempRows) {
+			startPoint = row.moveRow(startPoint);
 		}
 		return startPoint;
 	}
 
 	public void drawLine(int row, int startPoint) {
-		matrix[row][startPoint] = 1;
-		matrix[row][startPoint + 1] = 1;
+		tempRows[row].drawRows(startPoint);
 	}
+	
+//	public static void main(String[] args) {
+//		
+//	}
 }
--- Ladder/src/ladder/Row.java
+++ Ladder/src/ladder/Row.java
@@ -1,5 +1,35 @@
 package ladder;
 
 public class Row {
+	int[] rows;
+	
+	Row(int numOfRows){
+		rows = new int[numOfRows];
+	}
+	
+	void drawRows(int rowSeq) {
+		rows[rowSeq] = 1;
+		rows[rowSeq + 1] = 1;
+		for (int a=0; a<rows.length ; a++) {
+			System.out.print(rows[a]);
+		}
+	}
+	
+	int moveRow(int startPoint) {
+		if (isNoLine(startPoint)) {
+			return startPoint;
+		}
+		if (canMoveToRight(startPoint)) {
+			return startPoint + 1;
+		}
+		return startPoint - 1;
+	}
 
+	private boolean canMoveToRight(int startPoint) {
+		return startPoint < (rows.length-1) && rows[startPoint + 1] == 1;
+	}
+
+	private boolean isNoLine(int startPoint) {
+		return rows[startPoint] == 0;
+	}
 }
